8323d0d8391775495d927fa559b378aa428cdd4a
SarunasGustainis <sarunas@gustainis.lt>
2023-02-24 16:29:44 +0200

    jsx components added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+function FirstReactComponent() {
+  return <p>I am new here!</p>;
+}
+
+function ChildrenWithTitle(props) {
+  return (
+    <div>
+      <h2>{props.title}</h2>
+      {props.children}
+    </div>
+  );
+}
 
 function App() {
   return (
-    <>
-<h1>Hello world</h1>
-<p>You look beautiful</p>
-</>
+    <div>
+      <h1>Hello world</h1>
+      <p>You look beautiful</p>
+      <FirstReactComponent />
+      <ChildrenWithTitle title="I am title">I am children</ChildrenWithTitle>
+    </div>
   );
 }
 
